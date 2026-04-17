698b058086b19aa22b1c2febbd81d87a6b62670b
davidcolorado01 <david.colorado01@unicatolica.edu.co>
2026-04-17 15:55:36 -0500

    Cambios

---
+++ pila.js
@@ -0,0 +1,31 @@
+class Pila {
+    constructor() {
+        this.items = [];
+    }
+    
+    agregar(elemento) {
+        this.items.push(elemento);
+        console.log("Se agregó:", elemento);
+    }
+    
+    eliminar() {
+        if (this.items.length === 0) {
+            console.log("La pila está vacía");
+            return;
+        }
+        let eliminado = this.items.pop();
+        console.log("Se eliminó:", eliminado);
+    }
+    mostrar() {
+        console.log("Pila actual:", this.items);
+    }
+}
+
+let pila = new Pila();
+
+pila.agregar({ nombre: "Juliana", edad: 38 });
+pila.agregar({ nombre: "Andres", edad: 18 });
+pila.mostrar();
+pila.eliminar();
+pila.eliminar();
+pila.mostrar();
